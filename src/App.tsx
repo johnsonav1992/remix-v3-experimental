a097bbe95cf03564ec5e8a2e9571de1cd676b51d
Alex Johnson <johnsonav1992@gmail.com>
2025-11-15 20:14:01 -0600

    more features

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import type { Remix } from "@remix-run/dom";
 import { pressDown } from "@remix-run/events/press";
 
-export function App(this: Remix.Handle) {
+export function App(this: Remix.Handle<{ someContextValue: string }>) {
 	let count = 0;
 
+	this.context.set({
+		someContextValue: "hello from context",
+	});
+
 	return () => (
-		<button
-			type="button"
-			on={pressDown(() => {
-				count++;
-				count++;
-				this.update();
-			})}
-		>
-			Click Me {count}
-		</button>
+		<>
+			<button
+				type="button"
+				on={pressDown(() => {
+					count++;
+					count++;
+					this.update();
+				})}
+			>
+				Click Me {count}
+			</button>
+			<ChildComponent someProp="hey" />
+		</>
 	);
+}
+
+type ChildProps = {
+	someProp: string;
+};
+
+function ChildComponent(this: Remix.Handle, { someProp }: ChildProps) {
+	const context = this.context.get(App);
+
+	console.log(context.someContextValue);
+
+	return () => <div>I'm a child component {someProp} </div>;
 }
